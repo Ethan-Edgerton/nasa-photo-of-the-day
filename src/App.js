35345124c283d1f3fc6662a13237ffad5f6a72c3
Ethan-Edgerton <Ethan.E.Edgerton@gmail.com>
2020-03-03 22:03:10 -0600

    MVP met

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=J2vl4BfpMtjfJFYguA8tm3ZLrF5YjR7RTrH6FInk&date=2019-03-14")
+      .then((response) => {
+        //   console.log(response)
+           setNasaData(response.data)
+        })
+          .catch((err) => {console.log(err)})
+},[])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="container">
+        <div className="sub-container">
+          <div className="header"> 
+          
+          </div>
+          <div className="content">
+             <h1>{nasaData.title}</h1>
+            <h2>{nasaData.date}</h2>
+            <img className="apod" src={nasaData.url}></img>
+            <p>{nasaData.explanation}</p>
+          </div>
+      </div>
     </div>
+    {console.log(nasaData)}
+  </div>
   );
 }
 
